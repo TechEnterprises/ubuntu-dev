daeffc7c9b62e9072a64422f264b1469c9b7b321
Patrick Coffey <patrickcoffey48@gmail.com>
2015-09-01 15:11:43 -0500

    fix things, add paper ui

--- ubuntu-dev.sh
+++ ubuntu-dev.sh
@@ -1,14 +1,14 @@
 # Install git
-sudo apt-get install git
+#sudo apt-get install git
 
 # Clone ubuntu-dev repo
-git clone https://github.com/patrickocoffeyo/ubuntu-dev.git ~/.ubuntu-dev
+#git clone https://github.com/patrickocoffeyo/ubuntu-dev.git ~/.ubuntu-dev
 
 # Install ansible
-add-apt-repository ppa:rquillo/ansible
-apt-get update
-apt-get install ansible
+#add-apt-repository ppa:rquillo/ansible
+#apt-get update
+#apt-get install ansible
 
 # Run ansible-playbook
-cd ~/ubuntu-dev
-ansible-playbook -K ubuntu-dev.yml
+#cd ~/ubuntu-dev
+ansible-playbook -i "localhost," -c local ubuntu-dev.yml

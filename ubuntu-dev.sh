c8e34fdce145c65e9de37385d1b236968dfeda2e
Patrick Coffey <patrickcoffey48@gmail.com>
2016-02-18 21:21:22 -0600

    Add nfs

--- ubuntu-dev.sh
+++ ubuntu-dev.sh
@@ -1,7 +1,7 @@
 # Install ansible
-add-apt-repository ppa:rquillo/ansible
-apt-get update
-apt-get install ansible
+#add-apt-repository ppa:rquillo/ansible
+#apt-get update
+#apt-get install ansible
 
 # Run ansible-playbook
 ansible-playbook -i "localhost," -c local ubuntu-dev.yml --ask-sudo-pass
